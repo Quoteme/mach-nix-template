99909320b5f89d761a30cdcb2f8c1edfaa3c064d
Luca Leon Happel <luca.happel@uni-duesseldorf.de>
2022-02-23 17:47:01 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # This section will allow us to create a python environment
+    # with specific predefined python packages from PyPi
+    pypi-deps-db = {
+      url = "github:DavHau/mach-nix/3.3.0";
+    };
+    mach-nix = {
+      url = "github:DavHau/mach-nix/3.3.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.pypi-deps-db.follows = "pypi-deps-db";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }@attr:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      # specify the base version of python you with to use
+      # and prepare mach-nix for usage
+      mach = import mach-nix { inherit pkgs "python39"; };
+      
+      # create a custom python environment
+      myPythonb = (mach.mkPython {
+        requirements = ''
+          numpy
+          gkeepapi
+        '';
+      });
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [
+          # Now you can use your custom python environemt!
+          # This should also work for `buildInputs` and so on!
+          pythonEnviroment
+        ];
+      };
+    }
+    );
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Python template for nix flakes
