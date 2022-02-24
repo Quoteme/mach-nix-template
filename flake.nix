29e56554230b2bb56aaa398fa435bedaa05a4563
Luca Leon Happel <luca.happel@uni-duesseldorf.de>
2022-02-24 01:08:41 +0100

    trying to add template support still

--- flake.nix
+++ flake.nix
@@ -1,58 +1,15 @@
 {
-  description = "A very basic flake";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    # This section will allow us to create a python environment
-    # with specific predefined python packages from PyPi
-    pypi-deps-db = {
-      url = "github:DavHau/mach-nix/3.3.0";
+  description = ''
+  Please look inside the ./python folder. This file is only here, so
+  the template option of `nix flake new` works!
+  '';
+  outputs = {
+    templates = {
+      python = {
+        path = ./.;
+        description = "A simple python template using mach-nix";
+      };
     };
-    mach-nix = {
-      url = "github:DavHau/mach-nix/3.3.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.pypi-deps-db.follows = "pypi-deps-db";
-    };
+    defaultTemplate = self.templates.python;
   };
-
-  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }@attr:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      # specify the base version of python you with to use
-      # and prepare mach-nix for usage
-      python = "python39";
-      mach = import mach-nix { inherit pkgs python; };
-
-      
-      # create a custom python environment
-      myPython = (mach.mkPython {
-        requirements = ''
-          numpy
-          gkeepapi
-        '';
-      });
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [
-          # Now you can use your custom python environemt!
-          # This should also work for `buildInputs` and so on!
-          myPython
-        ];
-      };
-      # The following can be removed in your code. The code just
-      # makes nix able to use this repo as a template using
-      # nix flake new newRepoName -t github:Quoteme/mach-nix-template
-      templates = {
-        python = {
-          path = ./.;
-          description = "A simple python template using mach-nix";
-        };
-      };
-      defaultTemplate = self.templates.python;
-    }
-    );
 }
